d92cc63743d5945dcf5212f1a90093c093d7135a
NejoBMG <gonzalo.ulloa99@gmailcom>
2022-08-23 10:02:11 -0300

    Data Parsing

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const multer = require('multer')
 
 const app = express();
+
+// Middleware
+
+// Middleware para x-www-form-urlencoded
+app.use(express.urlencoded({
+    extended:true
+}))
+// Middleware para json (raw=>json)
+app.use(express.json())
+
+// Middleware para form-data
+app.use(multer().none())
+
+//  "/" index, home, root
 app.get("/",(peticion,respuesta)=>{
     console.log(peticion)
     
     return respuesta.end('Hola Mundo')
 });
 app.post("/saludar",(peticion,respuesta)=>{
-    console.log(peticion.body)
+    const name = peticion.body.name
 
-    return respuesta.status(201).send("<h1>hola</h1>");
+    return respuesta.status(201).send(`<h1>hola ${name}</h1>`);
 })
 app.listen(4000,()=>{
     console.log('Listening on http://localhost:4000');
 });
 
-// investigar acerca de form-data, x-www-form-urlencoded,application/json
+// investigar MVC(model view controller), arquitectura de software
